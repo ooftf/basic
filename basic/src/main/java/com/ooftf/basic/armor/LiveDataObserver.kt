bae08c1f26604f5e57b999719c0e459bc861a638
ooftf <li994749769@gmail.com>
2020-11-12 18:51:32 +0800

    LiveDataObserver  update

--- basic/src/main/java/com/ooftf/basic/armor/LiveDataObserver.kt
+++ basic/src/main/java/com/ooftf/basic/armor/LiveDataObserver.kt
@@ -2,6 +2,10 @@ package com.ooftf.basic.armor
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.ooftf.basic.utils.ThreadUtil
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 /**
  *
@@ -13,6 +17,26 @@ class LiveDataObserver<T> : MutableLiveData<T>() {
     fun <S> observeLiveData(source: LiveData<S>, adapter: (S) -> T) {
         source.observeForever {
             value = adapter.invoke(it)
+        }
+    }
+
+    fun <S> observeEachOther(source: MutableLiveData<S>, adapter: (S) -> T, adapter2: (T) -> S) {
+        source.observeForever {
+            val invoke = adapter.invoke(it)
+            if (value != invoke) {
+                ThreadUtil.runOnUiThread {
+                    value = invoke
+                }
+            }
+        }
+
+        this.observeForever {
+            val invoke = adapter2.invoke(it)
+            if (invoke != source.value) {
+                ThreadUtil.runOnUiThread {
+                    source.value = invoke
+                }
+            }
         }
     }
 }
