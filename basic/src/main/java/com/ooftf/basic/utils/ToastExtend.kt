dcea6fd2fbda8b9b1a76c6ed1a9336d8c72809dd
ooftf <li994749769@gmail.com>
2020-11-23 20:15:42 +0800

    toastImg

--- basic/src/main/java/com/ooftf/basic/utils/ToastExtend.kt
+++ basic/src/main/java/com/ooftf/basic/utils/ToastExtend.kt
@@ -2,8 +2,12 @@ package com.ooftf.basic.utils
 
 import android.content.Context
 import android.view.Gravity
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.DrawableRes
 import com.ooftf.basic.AppHolder
+import com.ooftf.basic.R
 
 /**
  *
@@ -15,11 +19,32 @@ var toastGravity: Int = Gravity.BOTTOM
 fun toast(
     message: CharSequence,
     context: Context = AppHolder.app,
-    duration: Int = Toast.LENGTH_SHORT
+    duration: Int = Toast.LENGTH_SHORT,
+    gravity: Int = toastGravity
 ) {
     val toast = Toast.makeText(context, message, duration)
-    if (toastGravity != Gravity.BOTTOM) {
-        toast.setGravity(toastGravity, 0, 0)
+    if (gravity != Gravity.BOTTOM) {
+        toast.setGravity(gravity, 0, 0)
+    }
+    toast.show()
+}
+
+
+fun toastImg(
+    message: CharSequence,
+    @DrawableRes imgId: Int,
+    context: Context = AppHolder.app,
+    duration: Int = Toast.LENGTH_SHORT,
+    gravity: Int = toastGravity
+) {
+    Toast(AppHolder.app).apply {
+        view = AppHolder.app.inflate(R.layout.basic_ooftf_toast_img)
+        view.findViewById<TextView>(R.id.text).text = message
+        view.findViewById<ImageView>(R.id.img).setImageResource(imgId)
+    }
+    val toast = Toast.makeText(context, message, duration)
+    if (gravity != Gravity.BOTTOM) {
+        toast.setGravity(gravity, 0, 0)
     }
     toast.show()
 }
