1fb642ce2a25132784cae29be89e35bbd6179ee8
ooftf <li994749769@gmail.com>
2021-03-01 17:54:34 +0800

    测试

--- app/src/main/java/com/ooftf/basic/holder/App.kt
+++ app/src/main/java/com/ooftf/basic/holder/App.kt
@@ -2,6 +2,7 @@ package com.ooftf.basic.holder
 
 import android.app.Application
 import android.util.Log
+import kotlin.concurrent.thread
 
 /**
  *
@@ -9,9 +10,22 @@ import android.util.Log
  * @email 994749769@qq.com
  * @date 2020/9/24
  */
-class App: Application() {
+class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        Log.e("loggggg","Application   onCreate"+toString())
+        Log.e("loggggg", "Application   onCreate" + toString())
+        val s = ThreadLocal<String>()
+        s.set("main")
+        thread {
+            Log.e("thread1-pre", ""+s.get())
+            s.set("thread1")
+            Log.e("thread1-after", ""+s.get())
+        }
+        thread {
+            Log.e("thread2-pre", ""+s.get())
+            s.set("thread2")
+            Log.e("thread2-after", ""+s.get())
+        }
+        Log.e("main-after", ""+s.get())
     }
 }
