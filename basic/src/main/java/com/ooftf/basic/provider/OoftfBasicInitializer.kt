566b9bb979db87775a73f81ccd7782fc232d161b
ooftf <li994749769@gmail.com>
2021-07-07 15:47:01 +0800

    优化代码

--- basic/src/main/java/com/ooftf/basic/provider/OoftfBasicInitializer.kt
+++ basic/src/main/java/com/ooftf/basic/provider/OoftfBasicInitializer.kt
@@ -14,7 +14,8 @@ import com.ooftf.basic.engine.ActivityManager
 class OoftfBasicInitializer : Initializer<ActivityManager> {
 
     override fun create(context: Context): ActivityManager {
-        ActivityManager.init(context as Application)
+
+        ActivityManager.init(context.applicationContext as Application)
         return ActivityManager
     }
 
